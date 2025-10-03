052e0cf16ae06b7c44e6613bf0e282e8dbb85a39
Trần Văn Trọng <160373827+trantrongnb@users.noreply.github.com>
2025-10-03 22:06:52 +0700

    update

--- utils/read_data.py
+++ utils/read_data.py
@@ -7,51 +7,18 @@ def read_data(file_path):
             'Hypertension', 'Diabetes', 'Placental_position',
             'Bleeding_first_trimester', 'Bleeding_second_trimester', 'Funneling', 'Smoker']
 
-    # with open(file_path,'r',encoding='utf-8') as f:
-    #     metadata_dict = {col: None for col in num_cols}
-    #     for line in f:
-    #         line_split=line.strip().split()
-    #         if line_split[0]=='#':
-    #             if line_split[1] in num_cols:
-    #                 metadata_dict[line_split[1]]=line_split[2]
-    #     if float(metadata_dict['Rectime'])*7>=26*7:
-    #         if float(metadata_dict['Gestation'])*7>=37*7:
-    #             label=0
-    #         else:
-    #             label=1
-    #         return metadata_dict,[label,float(metadata_dict['Gestation'])*7.0]
-    #     else:
-    #         return None,None
-
-    if file_path.endswith("m.hea"):
-        with open(file_path,'r',encoding='utf-8') as f:
-            metadata_dict = {col: None for col in num_cols}
-            for line in f:
-                line_split=line.strip().split()
-                if line_split[0]=='#':
-                    if line_split[1] in num_cols:
-                        metadata_dict[line_split[1]]=line_split[2]
-            if float(metadata_dict['Rectime'])*7>=26*7:
-                if float(metadata_dict['Gestation'])*7>=37*7:
-                    label=0
-                else:
-                    label=1
-                return metadata_dict,[label,float(metadata_dict['Gestation'])*7.0]
+    with open(file_path,'r',encoding='utf-8') as f:
+        metadata_dict = {col: None for col in num_cols}
+        for line in f:
+            line_split=line.strip().split()
+            if line_split[0]=='#':
+                if line_split[1] in num_cols:
+                    metadata_dict[line_split[1]]=line_split[2]
+        if float(metadata_dict['Rectime'])*7>=26*7:
+            if float(metadata_dict['Gestation'])*7>=37*7:
+                label=0
             else:
-                return None,None
-    else:
-        with open(file_path,'r',encoding='utf-8') as f:
-            metadata_dict = {col: None for col in num_cols}
-            for line in f:
-                line_split=line.strip().split()
-                if line_split[0]=='#':
-                    if line_split[1] in num_cols:
-                        metadata_dict[line_split[1]]=line_split[2]
-            if float(metadata_dict['Rectime'])*7>=26*7:
-                if float(metadata_dict['Gestation'])*7>=37*7:
-                    label=0
-                else:
-                    label=1
-                return metadata_dict,[label,float(metadata_dict['Gestation'])*7.0]
-            else:
-                return None,None
+                label=1
+            return metadata_dict,[label,float(metadata_dict['Gestation'])*7.0]
+        else:
+            return None,None
